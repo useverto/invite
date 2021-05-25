2a0060f7bce7d8d7beac5016828f6656da353111
John Letey <johnletey@gmail.com>
2021-05-25 19:14:28 +0100

    feat: implement the mint function

--- src/faces.ts
+++ src/faces.ts
@@ -12,3 +12,26 @@ export interface ActionInterface {
   input: any;
   caller: string;
 }
+
+// Module Faces
+
+export interface BalanceInterface {
+  function: "balance";
+  target?: string;
+}
+
+export interface InviteInterface {
+  function: "invite";
+  target: string;
+}
+
+export interface MintInterface {
+  function: "mint";
+  target: string;
+}
+
+export interface TransferInterface {
+  function: "transfer";
+  target: string;
+  qty: number;
+}
